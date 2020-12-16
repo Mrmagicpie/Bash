101988f216bbdd0d7bc2d6830b42505c2f353cd2
Mrmagicpie <70033286+Mrmagicpie@users.noreply.github.com>
2020-12-15 23:46:54 -0500

    Update updater.sh

--- updater/updater.sh
+++ updater/updater.sh
@@ -83,6 +83,26 @@ if [ "$updater_enabled" = "y" ] || [ "$updater_enabled" = "yes" ] || [ "$updater
                         if [ "$updater_logs" = "y" ] || [ "$updater_logs" = "Y" ]; then
                             echo "[INFO][$updater_time] Type 2 has been indicated! Begining update" >> "$updater_logs_location"
                         fi
+                        if ! [ -f "$dir/old-$0" ]; then
+                            mv "$dir/$0" "$dir/old-$0"
+                        else
+                            mv "$dir/$0" "$dir/$updater_time-$0"
+                        fi
+                        echo " "
+                        echo "A type 2 update has been indicated! This means the script will update itself! This is madated by your developer!"
+                        echo " "
+                        echo "Would you like to restart the script now or restart it on the next script start?"
+                        echo " "
+                        read -t 25 -p "Restart the script now? (y/n) " two_maybe
+
+                        if [ "$two_maybe" = "y" ] || [ "$two_maybe" = "Y" ] | [ "$two_maybe" = "yes" ]; then
+                            if [ "$updater_logs" = "y" ] || [ "$updater_logs" = "Y" ]; then
+                                echo "[INFO][$updater_time] Ending the script!" >> "$updater_logs_location"
+                            fi
+                            echo " "
+                            echo -e "Ending this script! Use$red bash $0$no_colour to restart!"
+                            echo " "
+                            exit
 
                     elif [ "$type" = "3" ]; then
                         # This is the last update type, download an updater and execute the update!
@@ -94,9 +114,10 @@ if [ "$updater_enabled" = "y" ] || [ "$updater_enabled" = "yes" ] || [ "$updater
                         # This is a catch-all for all non-defined options
                         if [ "$updater_logs" = "y" ] || [ "$updater_logs" = "Y" ]; then
                             echo -e "$red[FATAL][$updater_time] No type has been specified, exiting update!$no_colour" >> "$updater_logs_location"
-                        fi
-                    fi
-                fi
+                fi; fi; fi
+                #         fi
+                #     fi
+                # fi
             else
                 echo " "
                 echo "There is an update available for $resource!"
@@ -108,6 +129,7 @@ if [ "$updater_enabled" = "y" ] || [ "$updater_enabled" = "yes" ] || [ "$updater
                 if [ "$updater_logs" = "y" ] || [ "$updater_logs" = "Y" ]; then
                     echo -e "$red[FATAL][$updater_time] Your config/updater is not configured properly!$no_colour" >> "$updater_logs_location"
                     echo -e "$red[FATAL][$updater_time] Talk to your developer! Or reach out on GitHub!$no_colour" >> "$updater_logs_location"
+                    echo -e "$red[FATAL][$updater_time]     https://github.com/Mrmagicpie/Bash$no_colour" >> "$updater_logs_location"
                 fi
             fi
         fi
@@ -124,11 +146,3 @@ if [ "$updater_enabled" = "y" ] || [ "$updater_enabled" = "yes" ] || [ "$updater
         fi
     fi
 fi
-
-
-    
-
-    
-
-
-
